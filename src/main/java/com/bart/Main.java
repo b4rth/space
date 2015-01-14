9450b1cac81537e8cbe875e3bc82659eb3ea16aa
b4rth <b4rth@users.noreply.github.com>
2015-01-14 20:15:34 +0100

    project creation

--- src/main/java/com/bart/Main.java
+++ src/main/java/com/bart/Main.java
@@ -7,7 +7,7 @@ package com.bart;
 public class Main  {
  
     public static void main(String[] args){
-    	System.out.println("TEST");
+    	System.out.println("Space");
     	Application app = new Application();
         app.start(); // start the game
     }
