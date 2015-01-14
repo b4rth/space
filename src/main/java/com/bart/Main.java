ee28e69737c29891d4ab4521806144e70d06c946
b4rth <b4rth@users.noreply.github.com>
2015-01-14 20:07:06 +0100

    project creation

---
+++ src/main/java/com/bart/Main.java
@@ -0,0 +1,16 @@
+package com.bart;
+
+ 
+/** Sample 1 - how to get started with the most simple JME 3 application.
+ * Display a blue 3D cube and view from all sides by
+ * moving the mouse and pressing the WASD keys. */
+public class Main  {
+ 
+    public static void main(String[] args){
+    	System.out.println("TEST");
+    	Application app = new Application();
+        app.start(); // start the game
+    }
+    
+
+}
